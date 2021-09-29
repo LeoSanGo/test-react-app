b52c7e6132fb86dc5971532e969a3445bdc13e54
LeoSanGo <souoleo@gmail.com>
2021-09-29 13:50:09 -0300

    Components test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./styles.css";
-import ReactImage from "./img/react-blue.png";
+import Header from "./components/Header";
 
 export default function App() {
   const name = "Dionga";
   const funcao = () => {
     return "Retorno de função";
   };
+
   return (
     <div className="App">
+      <Header />
       <h1>Hello Test</h1>
       <h2>Hello {name}</h2>
       <h1>{funcao()}</h1>
-      <img src="/img/react.png" alt="react" />
-      <img src={ReactImage} alt="reactimg" />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "./img/react-blue.png";
+export default function Header() {
+  return (
+    <>
+      <h1>Component Header</h1>
+      <h2>Content</h2>
+      <img src={Logo} alt="" />
+    </>
+  );
+}
