5ebd19f3740d79587df6d5555f6884d4935995a4
LeoSanGo <souoleo@gmail.com>
2021-09-29 10:56:45 -0300

    Testing react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
+import ReactImage from "./img/react-blue.png";
 
 export default function App() {
+  const name = "Dionga";
+  const funcao = () => {
+    return "Retorno de função";
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Test</h1>
+      <h2>Hello {name}</h2>
+      <h1>{funcao()}</h1>
+      <img src="/img/react.png" alt="react" />
+      <img src={ReactImage} alt="reactimg" />
     </div>
   );
 }
